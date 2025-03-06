a6defe67607dceeeef07764a5039beabf12eae3a
pythonpaye <pkialain@cardinals.nfl.net>
2025-03-06 16:17:56 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()  # Get the incoming JSON data
+    print(f"Received request body: {json.dumps(data)}")  # Log the payload
+    return "Received data successfully", 200
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)  # Cloud Run expects the app to listen on port 8080
